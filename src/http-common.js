9c5b67a6dd458e1895379f0858a1e382ce679322
cwilliam98 <cristianwillian174@gmail.com>
2020-09-04 11:32:46 -0300

    novo commit

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: 'http://localhost:8081/',
+  baseURL: 'https://grades-api-cwilliam.herokuapp.com:35652',
   headers: {
     'Content-type': 'application/json',
   },
